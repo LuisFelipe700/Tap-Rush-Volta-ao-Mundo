6416789b820597ee196eb424a79d3a9649203044
Hugo Guilherme <hugojesus196@gmail.com>
2025-09-11 17:01:59 -0300

    unity cloud

--- Assets/Scripts/ResultadoUI.cs
+++ Assets/Scripts/ResultadoUI.cs
@@ -8,10 +8,34 @@ public class ResultadoUI : MonoBehaviour
 
     void Start()
     {
-        int pontuacaoFinal = GameManager.Instance.score;
-        int recorde = PlayerPrefs.GetInt("Record", 0);
+        // Certifica que o GameManager existe antes de tentar acessar o score
+        if (GameManager.Instance != null)
+        {
+            int pontuacaoFinal = GameManager.Instance.score;
+            int recorde = PlayerPrefs.GetInt("Record", 0);
 
-        textoPontuacao.text = "Pontuação Final: " + pontuacaoFinal;
-        textoRecorde.text = "Recorde: " + recorde;
+            if (textoPontuacao != null)
+            {
+                textoPontuacao.text = "Pontuação Final: " + pontuacaoFinal;
+            }
+
+            if (textoRecorde != null)
+            {
+                textoRecorde.text = "Recorde: " + recorde;
+            }
+        }
+        else
+        {
+            // Este código é útil para testar a cena de resultado sem o GameManager
+            if (textoPontuacao != null)
+            {
+                textoPontuacao.text = "Pontuação Final: 0";
+            }
+
+            if (textoRecorde != null)
+            {
+                textoRecorde.text = "Recorde: 0";
+            }
+        }
     }
 }
